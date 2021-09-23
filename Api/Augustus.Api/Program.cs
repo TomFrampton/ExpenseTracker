b0a63194bef36c8b0dea402fcf8a7fddaf8ff721
Tom Frampton <thomas.a.frampton@gmail.com>
2021-09-23 11:54:31 +0100

    Added basic Application Insights logging

--- Api/Augustus.Api/Program.cs
+++ Api/Augustus.Api/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Augustus.Api
 {
@@ -18,6 +13,12 @@ namespace Augustus.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    // Don't need to pass the instrumentation key.
+                    // Will automatically use the key environment variable in production.
+                    logging.AddApplicationInsights();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
